301cd7a62411936c2d86aad37565f4516bfcbf8d
Geanfranco Biagi <118919877+gbiagi@users.noreply.github.com>
2025-06-18 16:50:37 +0200

    register screen progress

--- composeApp/src/commonMain/kotlin/sdev/mediamaster/screens/RegisterScreen.kt
+++ composeApp/src/commonMain/kotlin/sdev/mediamaster/screens/RegisterScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
